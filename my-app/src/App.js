464235a2bf291342b583738144f0bbae1e307734
ElenaChrchk <hart03hapl@gmail.com>
2023-04-06 03:01:00 +0300

    React app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "./button/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header" style={{backgroundColor: "greenyellow"}}>Header level 1</h1>
+      <div>Test div</div>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
+
